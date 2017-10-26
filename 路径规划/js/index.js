5c5bd61815e3a4af7cd37ca654e97a54034e6061
zhao-bi-han <1171893698@qq.com>
2017-10-26 21:03:58 +0800

    优化js代码

--- 路径规划/js/index.js
+++ 路径规划/js/index.js
@@ -1,69 +1,83 @@
 // 初始化
-let panelEle=document.getElementById('panel');
-let startEle=document.getElementById('start');
-let endEle=document.getElementById('end');
-let searchEle=document.getElementById('search');
-let wayEle=document.getElementById('way');
-let aEle=wayEle.getElementsByTagName('a');
-let cityName="保定市";
-let way="Walking";
+let panelEle = document.getElementById('panel');
+let startEle = document.getElementById('start');
+let endEle = document.getElementById('end');
+let searchEle = document.getElementById('search');
+let wayEle = document.getElementById('way');
+let aEle = wayEle.getElementsByTagName('a');
+let way;
 
-let map = new AMap.Map('container', {
-    zoom: 12
-});
-// 获取当前城市
-window.onload=()=>{
-    map.getCity((data)=>{
-        cityName=data['city'] ;
+
+// 创建地图 及获取当前城市
+    let map = new AMap.Map('container', {
+        zoom: 12
     });
-}
+    map.getCity((data) => {
+        cityName = data['city'];
+    });
+
 
 // 方式选择
-wayEle.onclick=(e)=>{
-    for(let i=0;n=aEle.length,i<n;i++){
-          aEle[i].setAttribute('class','');
+wayEle.onclick = (e) => {
+    for (let i = 0; n = aEle.length, i < n; i++) {
+        aEle[i].setAttribute('class', '');
     }
-var target=e.target || e.srcElement;
-if(target.nodeName.toLowerCase()=='a'){
-    target.setAttribute('class','active');
-    way=target.getAttribute('item');
+    var target = e.target || e.srcElement;
+    if (target.nodeName.toLowerCase() == 'a') {
+        target.setAttribute('class', 'active');
+        way = target.getAttribute('item');    // 获取方式
 
- }
+    }
 }
-    
-// 添加事件
-searchEle.onclick=()=>{
-     panelEle.innerHTML=null;
-    let startVal=startEle.value;   // 获取input框的值
-    let endVal=endEle.value;
-    if(startVal!=''&&endVal!=''){  
+
+// 路径查询
+searchEle.onclick = () => {
+    panelEle.innerHTML = null;
+     new PathPlan({
+        startVal: startEle.value,  // 获取input框的值
+        endVal: endEle.value,
+        way:way
+    })
+}
+
+
+function PathPlan(options) {
+    this.opts = Object.assign({}, PathPlan.defaluts,options)
+    this.obj = {
+        map: map,
+        panel: "panel"    // 结果列表在此容器展示
+    }
+    this.init();
+}
+// 设置默认参数
+PathPlan.defaluts = {
+    cityName: "保定市",
+    way: "Walking",
+    startVal: null,
+    endVal: null,
+}
+
+PathPlan.prototype.init = function() {
+  let  startVal=this.opts.startVal;
+  let  endVal=this.opts.endVal;
+    if (startVal != '' && endVal != '') {
         map.clearMap();    // 清除路线 
-        query(way,startVal, endVal);
+        this.query(this.opts.way, this.opts.cityName, startVal,endVal);
     }
 }
-
-const query=(ways,start,end)=>{
-    map.plugin([`AMap.${ways}`], ()=>{              
-    var w;
-    if(ways=="Walking"){   // 判断是 步行、驾车、公交
-        w = new AMap.Walking(obj)
-    }else if(ways=="Transfer"){
-        w = new AMap.Transfer(obj)
-    }else{
-        w = new AMap.Driving(obj)
-    }
-    w.search([
-        { keyword: start,city:cityName},
-        { keyword: end ,city:cityName}
-    ])
-    // AMap.event.addListener(w, 'complete', function (e) {
-    //     console.log(e);
-    // })
-
-})
-}
-
-const obj={
-    map: map,
-    panel: "panel"    // 结果列表在此容器展示
+PathPlan.prototype.query =function (ways, cityName, start, end){
+    map.plugin([`AMap.${ways}`], () => {
+        let w;
+        if (ways == "Walking") {   // 判断是 步行、驾车、公交
+            w = new AMap.Walking(this.obj)
+        } else if (ways == "Transfer") {
+            w = new AMap.Transfer(this.obj)
+        } else {
+            w = new AMap.Driving(this.obj)
+        }
+        w.search([
+            { keyword: start, city: cityName },
+            { keyword: end, city: cityName }
+        ])
+    })
 }
